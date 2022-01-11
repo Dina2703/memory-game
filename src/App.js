a15f9f82531456e3f4f569c935abf45ecd2d6f7d
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-01-11 14:30:23 -0500

    Matching Cards

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import "./App.css";
 import Card from "./components/Card";
 
 const cardImages = [
-  { src: "./img/helmet-1.png" },
-  { src: "./img/potion-1.png" },
-  { src: "/img/ring-1.png" },
-  { src: "/img/scroll-1.png" },
-  { src: "/img/shield-1.png" },
-  { src: "/img/sword-1.png" },
+  { src: "./img/potion-1.png", matched: false },
+  { src: "./img/helmet-1.png", matched: false },
+  { src: "/img/ring-1.png", matched: false },
+  { src: "/img/scroll-1.png", matched: false },
+  { src: "/img/shield-1.png", matched: false },
+  { src: "/img/sword-1.png", matched: false },
 ];
 function App() {
   const [cards, setCards] = useState([]);
@@ -37,14 +37,23 @@ function App() {
   useEffect(() => {
     if (choiceOne && choiceTwo) {
       if (choiceOne.src === choiceTwo.src) {
-        console.log("they the same");
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
         resetTurn();
       } else {
-        console.log("they are differ");
         resetTurn();
       }
     }
   }, [choiceOne, choiceTwo]);
+
+  console.log(cards);
 
   //reset choices & increase turn
   const resetTurn = () => {
